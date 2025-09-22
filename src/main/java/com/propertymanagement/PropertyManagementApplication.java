adb5aa40d7c8bfc28c51b0b2e40df817e853aeff
nadhamsaipallavi <nadhamsaipallavi@gmail.com>
2025-09-22 15:03:12 +0530

    dscx

--- src/main/java/com/propertymanagement/PropertyManagementApplication.java
+++ src/main/java/com/propertymanagement/PropertyManagementApplication.java
@@ -3,19 +3,11 @@ package com.propertymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-public class PropertyManagementApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        // Needed for external Tomcat deployment
-        return application.sources(PropertyManagementApplication.class);
-    }
+public class PropertyManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PropertyManagementApplication.class, args);

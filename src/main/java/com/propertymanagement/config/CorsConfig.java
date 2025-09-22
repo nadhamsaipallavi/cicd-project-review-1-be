edea48382b7af58b8ac4319bca0563ee065d0bf9
nadhamsaipallavi <nadhamsaipallavi@gmail.com>
2025-09-22 17:08:56 +0530

    wdscx

---
+++ src/main/java/com/propertymanagement/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.propertymanagement.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Allow CORS for all endpoints
+                        .allowedOrigins("http://localhost:9090") // Frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+

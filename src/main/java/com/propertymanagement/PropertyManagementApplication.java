a39f8c3c8402f7bed254dd27dfa3899721a2236b
Nadham sai pallavi <143820290+nadhamsaipallavi@users.noreply.github.com>
2025-09-23 07:25:44 +0530

    Update PropertyManagementApplication.java

--- src/main/java/com/propertymanagement/PropertyManagementApplication.java
+++ src/main/java/com/propertymanagement/PropertyManagementApplication.java
@@ -3,13 +3,20 @@ package com.propertymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-public class PropertyManagementApplication {
+public class PropertyManagementApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PropertyManagementApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PropertyManagementApplication.class);
     }
 }
